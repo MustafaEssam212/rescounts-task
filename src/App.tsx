a63dc9d297b3f08d09b0a3758215da077623b3f5
Mustafa Essam <zamalekzamalek62@yahoo.com>
2025-01-31 18:40:01 +0200

    task

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import '@mui/material/styles';
+import DataGridDemo from './components/DataGridDemo';
+function App() {
+
+
+  return (
+    <>
+      <DataGridDemo />
+    </>
+  )
+}
+
+export default App
